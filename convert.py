29b2cec7b3acf38892546ea80b2aade329179c1f
LiamGraham <liam.david.graham@gmail.com>
2020-03-20 21:37:13 +1000

    Initial commit

---
+++ convert.py
@@ -0,0 +1,148 @@
+"""
+Script to convert Trello JSON data to user story template format.
+
+Story attributes:
+- Story title
+- Story ID
+- Priority
+- Story points
+- Story body
+- Acceptance criteria
+- Notes
+
+Trello card format:
+"(<story-points>) <title>: <story body>"
+e.g. "(2) Basic Entry Form: As a potential customer, I want to..."
+
+Trello description format:
+"
+## Acceptance Criteria
+- 
+- 
+...
+- 
+
+## Notes
+-
+-
+...
+- 
+"
+
+- Specify priority with lists
+- Add role as a tag (not a part of template)
+- Generate story ID based on ordering
+
+JSON fields:
+- desc - Description (acceptance criteria and notes)
+- name - Story content (points, title and body)
+
+Steps:
+- Load provided JSON file
+- 
+"""
+
+import slides
+
+from dataclasses import dataclass
+import sys
+import json
+from typing import List, Dict
+import re
+
+
+PRIORITIES = {
+    "Must Have": "M",
+    "Should Have": "S",
+    "Could Have": "C",
+    "Won't Have": "W",
+}
+
+
+CARD_PATTERN = r"\(([0-9^\s]+)\) ([\S\s^:]+): ([\S\s]+)"
+CRITERIA_PATTERN = r"## Acceptance Criteria\n(- [\S\s^\n]+[\n])+"
+NOTES_PATTERN = r"## Notes\n(- [\w\s^\n]+[\n]*)+"
+
+CARD_REGEX = re.compile(CARD_PATTERN)
+CRITERIA_REGEX = re.compile(CRITERIA_PATTERN)
+NOTES_REGEX = re.compile(NOTES_PATTERN)
+
+
+@dataclass
+class UserStory:
+    id_: str
+    title: str
+    body: str
+    priority: str
+    points: str
+    criteria: str=""
+    notes: str=""
+
+
+def validate_card(card: dict) -> bool:
+    return CARD_REGEX.match(card["name"]) is not None
+
+
+def parse_card(card: dict, lists: dict) -> UserStory:
+    content = card["name"]
+    desc = card["desc"]
+    id_ = str(card["idShort"])
+    priority = PRIORITIES[lists[card["idList"]]]
+
+    match = CARD_REGEX.match(content)
+    points, title, body = match.groups()
+
+    return UserStory(id_, title, body, priority, points) 
+
+
+def collect_lists(data: List[dict]) -> dict:
+    """Returns a mapping of Trello list ids to list names. Facilitates list name lookup where only the list id is given.
+    
+    Arguments:
+        data {List[dict]} -- Trello JSON data
+    
+    Returns:
+        dict -- Mapping of Trello list ids to list names
+    """
+    return {x["id"]:x["name"] for x in data["lists"]}
+
+
+def collect_stories(data: List[dict]) -> List[UserStory]:
+    """Returns a list of UserStory objects extracted from the given Trello JSON data.
+    
+    Arguments:
+        data {List[dict]} -- JSON data from which stories will be extracted
+    
+    Returns:
+        List[UserStory] -- List of parsed UserStory objects
+    """
+    cards = data["cards"]
+    lists = collect_lists(data)
+    stories = []
+    
+    for card in cards:
+        if lists[card["idList"]] not in PRIORITIES:
+            continue
+        if not validate_card(card):
+            print(f"Card is not valid. Check that it has the correct format: \"{card['name']}\"")
+            continue 
+        stories.append(parse_card(card, lists))
+    return stories
+
+
+def main(filename: str):
+    """Main process
+    
+    Arguments:
+        filename {str} -- Name of JSON file to be converted
+    """
+    with open(filename, "r") as f:
+        data = json.load(f)
+    stories = collect_stories(data)
+    slides.create_slides(stories)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        raise ValueError("Must provide path to JSON file")
+    main(sys.argv[1])
---
+++ slides.py
@@ -0,0 +1,302 @@
+from pptx import Presentation
+from pptx.util import Cm, Pt
+from pptx.enum.shapes import MSO_SHAPE
+from pptx.enum.dml import MSO_THEME_COLOR
+from pptx.enum.text import MSO_AUTO_SIZE
+from pptx.enum.text import MSO_VERTICAL_ANCHOR
+from pptx.enum.text import PP_ALIGN
+from pptx.dml.color import RGBColor
+
+
+prs = Presentation()
+
+SLD_LAYOUT_BLANK = 6
+BULLET  = chr(8226)
+
+
+def create_slides(stories):
+    for story in stories:
+        print(story)
+
+        slide_layout = prs.slide_layouts[SLD_LAYOUT_BLANK]
+        slide = prs.slides.add_slide(slide_layout)
+        shapes = slide.shapes
+        
+        left = Cm(0.21)
+        top = Cm(0.24)
+        width = Cm(2.49)
+        height = Cm(1.93)
+
+
+        shape = shapes.add_shape(
+           MSO_SHAPE.RECTANGLE, left, top, width, height
+           )
+
+        fill = shape.fill
+        fill.solid()
+        fill.fore_color.theme_color = MSO_THEME_COLOR.ACCENT_1
+        fill.fore_color.brightness = 0.4
+
+        line = shape.line
+        line.color.theme_color = MSO_THEME_COLOR.ACCENT_1
+        line.color.brightness = -0.5 
+        line.width = Pt(2)
+
+        text_frame = shape.text_frame
+        text_frame.auto_size = MSO_AUTO_SIZE.TEXT_TO_FIT_SHAPE
+        text_frame.word_wrap = True
+        text_frame.vertical_anchor = MSO_VERTICAL_ANCHOR.MIDDLE
+        text_frame.margin_top = Cm(0.13)
+        text_frame.margin_bottom = Cm(0.13)
+
+
+        p = text_frame.paragraphs[0]
+        p.alignment = PP_ALIGN.LEFT
+        p.font.bold = False
+        p.font.color.rgb = RGBColor(0,0,0)
+        p.text = 'Story ID: ' + story.id_
+
+        ######################### STORY_TITLE
+        left = Cm(3.02)
+        top = Cm(0.24)
+        width = Cm(15.62)
+        height = Cm(2.05)
+
+        shape = shapes.add_shape(
+           MSO_SHAPE.RECTANGLE, left, top, width, height
+           )
+
+        fill = shape.fill
+        fill.solid()
+        fill.fore_color.theme_color = MSO_THEME_COLOR.ACCENT_1
+
+
+        line = shape.line
+        line.color.theme_color = MSO_THEME_COLOR.ACCENT_1
+        line.color.brightness = -0.5 
+        line.width = Pt(2)
+
+        text_frame = shape.text_frame
+        text_frame.auto_size = MSO_AUTO_SIZE.TEXT_TO_FIT_SHAPE
+        text_frame.vertical_anchor = MSO_VERTICAL_ANCHOR.MIDDLE
+        text_frame.margin_top = Cm(0.13)
+        text_frame.margin_bottom = Cm(0.13)
+        text_frame.word_wrap = True
+
+
+        p = text_frame.paragraphs[0]
+        p.alignment = PP_ALIGN.CENTER
+        p.font.bold = False
+        p.font.color.rgb = RGBColor(0,0,0)
+        p.text = story.title
+
+        ######################### PRIORITY
+
+        left = Cm(18.96)
+        top = Cm(0.24)
+        width = Cm(3.01)
+        height = Cm(1.93)
+
+
+        shape = shapes.add_shape(
+           MSO_SHAPE.RECTANGLE, left, top, width, height
+           )
+
+        fill = shape.fill
+        fill.solid()
+        fill.fore_color.theme_color = MSO_THEME_COLOR.ACCENT_1
+        fill.fore_color.brightness = 0.4
+
+        line = shape.line
+        line.color.theme_color = MSO_THEME_COLOR.ACCENT_1
+        line.color.brightness = -0.5 
+        line.width = Pt(2)
+
+        text_frame = shape.text_frame
+        text_frame.auto_size = MSO_AUTO_SIZE.TEXT_TO_FIT_SHAPE
+        text_frame.word_wrap = True
+        text_frame.vertical_anchor = MSO_VERTICAL_ANCHOR.MIDDLE
+        text_frame.margin_top = Cm(0.13)
+        text_frame.margin_bottom = Cm(0.13)
+
+
+        p = text_frame.paragraphs[0]
+        p.alignment = PP_ALIGN.CENTER
+        p.font.bold = False
+        p.font.color.rgb = RGBColor(0,0,0)
+        p.text = 'Priority: '+ story.priority
+
+        ######################### STORY POINTS
+
+
+        left = Cm(22.1)
+        top = Cm(0.24)
+        width = Cm(3.16)
+        height = Cm(1.93)
+
+
+        shape = shapes.add_shape(
+           MSO_SHAPE.RECTANGLE, left, top, width, height
+           )
+
+        fill = shape.fill
+        fill.solid()
+        fill.fore_color.theme_color = MSO_THEME_COLOR.ACCENT_1
+        fill.fore_color.brightness = 0.4
+
+        line = shape.line
+        line.color.theme_color = MSO_THEME_COLOR.ACCENT_1
+        line.color.brightness = -0.5 
+        line.width = Pt(2)
+
+        text_frame = shape.text_frame
+        text_frame.auto_size = MSO_AUTO_SIZE.TEXT_TO_FIT_SHAPE
+        text_frame.word_wrap = True
+        text_frame.vertical_anchor = MSO_VERTICAL_ANCHOR.MIDDLE
+        text_frame.margin_top = Cm(0.13)
+        text_frame.margin_bottom = Cm(0.13)
+
+
+        p = text_frame.paragraphs[0]
+        p.alignment = PP_ALIGN.CENTER
+        p.font.bold = False
+        p.font.color.rgb = RGBColor(0,0,0)
+        p.text = 'Story Points: ' + story.points
+
+        ######################### STORY
+
+
+        left = Cm(0.21)
+        top = Cm(2.41)
+        width = Cm(24.98)
+        height = Cm(7.4)
+
+
+        shape = shapes.add_shape(
+           MSO_SHAPE.RECTANGLE, left, top, width, height
+           )
+
+        fill = shape.fill
+        fill.solid()
+        fill.fore_color.rgb = RGBColor(109,158,235)
+
+        line = shape.line
+        line.color.theme_color = MSO_THEME_COLOR.ACCENT_1
+        line.color.brightness = -0.5 
+        line.width = Pt(2)
+
+        text_frame = shape.text_frame
+        text_frame.auto_size = MSO_AUTO_SIZE.TEXT_TO_FIT_SHAPE
+        text_frame.word_wrap = True
+        text_frame.vertical_anchor = MSO_VERTICAL_ANCHOR.TOP
+        text_frame.margin_top = Cm(0.13)
+        text_frame.margin_bottom = Cm(0.13)
+        text_frame.margin_left = Cm(0.13)
+        text_frame.margin_right = Cm(0.13)
+
+
+        p = text_frame.paragraphs[0]
+        p.font.color.rgb = RGBColor(0,0,0)
+        p.alignment = PP_ALIGN.LEFT
+        p.text = '\t'+ story.body
+
+
+        ######################### ACCEPTANCE CRITERIA
+
+
+        left = Cm(0.21)
+        top = Cm(10.03)
+        width = Cm(24.98)
+        height = Cm(4.67)
+
+
+        shape = shapes.add_shape(
+           MSO_SHAPE.RECTANGLE, left, top, width, height
+           )
+
+        fill = shape.fill
+        fill.solid()
+        fill.fore_color.rgb = RGBColor(109,158,235)
+
+        line = shape.line
+        line.color.theme_color = MSO_THEME_COLOR.ACCENT_1
+        line.color.brightness = -0.5 
+        line.width = Pt(2)
+
+        text_frame = shape.text_frame
+        text_frame.auto_size = MSO_AUTO_SIZE.TEXT_TO_FIT_SHAPE
+        text_frame.word_wrap = True
+        text_frame.vertical_anchor = MSO_VERTICAL_ANCHOR.TOP
+        text_frame.margin_top = Cm(0.13)
+        text_frame.margin_bottom = Cm(0.13)
+        text_frame.margin_left = Cm(0.13)
+        text_frame.margin_right = Cm(0.13)
+
+
+        p = text_frame.paragraphs[0]
+        p.alignment = PP_ALIGN.LEFT
+        p.font.bold = False
+        p.font.color.rgb = RGBColor(0,0,0)
+        p.text = 'Acceptance Criteria:'
+
+
+        p = text_frame.add_paragraph()
+        p.font.color.rgb = RGBColor(0,0,0)
+
+        paragraph_strs = story.criteria.split('\n')
+        p.text = '\t'+BULLET+' '+paragraph_strs[0]
+        for para_str in paragraph_strs[1:]:
+            p = text_frame.add_paragraph()
+            p.font.color.rgb = RGBColor(0,0,0)
+            p.text = '\t'+BULLET+' '+para_str
+
+        ######################### NOTES
+
+
+        left = Cm(0.21)
+        top = Cm(14.92)
+        width = Cm(24.98)
+        height = Cm(3.64)
+
+
+        shape = shapes.add_shape(
+           MSO_SHAPE.RECTANGLE, left, top, width, height
+           )
+
+        fill = shape.fill
+        fill.solid()
+        fill.fore_color.rgb = RGBColor(255,255,255)
+
+        line = shape.line
+        line.color.theme_color = MSO_THEME_COLOR.ACCENT_1
+        line.color.brightness = -0.5 
+        line.width = Pt(.75)
+
+        text_frame = shape.text_frame
+        text_frame.auto_size = MSO_AUTO_SIZE.TEXT_TO_FIT_SHAPE
+        text_frame.word_wrap = True
+        text_frame.vertical_anchor = MSO_VERTICAL_ANCHOR.MIDDLE
+        text_frame.margin_top = Cm(0.13)
+        text_frame.margin_bottom = Cm(0.13)
+        text_frame.margin_left = Cm(0.13)
+        text_frame.margin_right = Cm(0.13)
+
+
+        p = text_frame.paragraphs[0]
+        p.alignment = PP_ALIGN.LEFT
+        p.font.bold = False
+        p.font.color.rgb = RGBColor(0,0,0)
+        p.text = 'Notes:'
+        p = text_frame.add_paragraph()
+        p.font.color.rgb = RGBColor(0,0,0)
+        
+
+        paragraph_strs = story.notes.split('\n')
+        p.text = '\t'+BULLET+' '+paragraph_strs[0]
+        for para_str in paragraph_strs[1:]:
+            p = text_frame.add_paragraph()
+            p.font.color.rgb = RGBColor(0,0,0)
+            p.text = '\t'+BULLET+' '+para_str
+
+
+    prs.save('example.pptx')
